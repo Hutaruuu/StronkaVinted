3165c8c77dde4cbffe53a4d00d5afd0fe9dd7829
Hutaruuu <gracjand25@gmail.com>
2025-02-13 23:52:25 +0100

    malezmiany

--- cos.py
+++ cos.py
@@ -48,4 +48,5 @@ def upload_file():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render przydziela dynamiczny port
+    app.run(host="0.0.0.0", port=port)
